59d72229afd70770c1c7846b077fd6cdc790c48c
maxx.diego <77diego.max@gmail.com>
2024-11-20 18:15:48 -0300

    @maxxdiego

---
+++ 06-vue.js-props/src/components/ClienteComp.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="cliente">
+    <h4>Nome: {{ cliente.nome }}</h4>
+    <p>E-mail: {{ cliente.email }}</p>
+    <p>Idade: {{ cliente.idade }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    nome: String,
+    email: String,
+    idade: Number,
+    cliente: Object,
+  },
+};
+</script>
+
+<style scoped>
+#cliente {
+  background-color: #ececec;
+  max-width: 600px;
+  padding: 1%;
+  margin-top: 2%;
+}
+
+/* #clienteNome {
+  color: white;
+} */
+</style>
