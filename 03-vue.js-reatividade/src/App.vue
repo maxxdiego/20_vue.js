4eca486dd17f831520d7e4500bcb578deb117b44
maxx.diego <77diego.max@gmail.com>
2024-11-20 16:44:37 -0300

    @maxxdiego

---
+++ 03-vue.js-reatividade/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Clientes:</h1>
+    <ClienteComp/>
+    <hr>
+      <h2>Lista de produtos:</h2>
+    <hr>
+    <ProdutoComp/>
+    <ProdutoComp/>
+  </div>
+</template>
+
+<script>
+import ClienteComp from "./components/ClienteComp.vue";
+import ProdutoComp from "./components/ProdutoComp.vue";
+
+export default {
+  name: "App",
+  components: {
+    ClienteComp,
+    ProdutoComp
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 03-vue.js-reatividade/src/components/ClienteComp.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="cliente">
+    <h2 id="clienteNome">{{ nome }}</h2>
+    <p>Descrição do cliente: {{ descricao }}</p>
+    <!-- Expressões Javascript -->
+    {{ 6 }}
+    {{ 2 + 2 * 2 + 2 }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: "Diego Max",
+      numero: "",
+      email: "",
+      idade: 0,
+      descricao: "Lorem ipsum dolor sit amet",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#cliente {
+  /* color: green; */
+  /* background-color: black; */
+  max-width: 600px;
+  height: 120px;
+}
+/* #clienteNome {
+  color: white;
+} */
+</style>
