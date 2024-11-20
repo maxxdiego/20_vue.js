c70480c10a374f7b9067fffe16eef322ae968d28
maxx.diego <77diego.max@gmail.com>
2024-11-20 16:21:06 -0300

    @maxxdiego

---
+++ 02-vue.js-componentes/src/components/ClienteComp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="cliente">
+    <h2 id="clienteNome">Cliente nome</h2>
+    <h3>Descrição do cliente</h3>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#cliente {
+  color: green;
+  background-color: black;
+  max-width: 600px;
+  height: 120px;
+}
+#clienteNome {
+  color: white;
+}
+</style>
