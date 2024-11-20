b80d35bfb46928156fb19813481d4fbc4bb5ca80
maxx.diego <77diego.max@gmail.com>
2024-11-20 17:40:02 -0300

    @maxxdiego

--- 04-vue.js-data-binding-one-way-binding/src/components/ClienteComp.vue
+++ 04-vue.js-data-binding-one-way-binding/src/components/ClienteComp.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="cliente">
+    <!-- Data binding (One-Way Binding) -->
+     <input type="text" :value="nome">
+     <hr>
+     <input type="text" :value="descricao">
+
     <h2 id="clienteNome">{{ nome }}</h2>
     <p>Descrição do cliente: {{ descricao }}</p>
     <!-- Expressões Javascript -->
@@ -23,12 +28,13 @@ export default {
 </script>
 
 <style scoped>
-#cliente {
-  /* color: green; */
-  /* background-color: black; */
+/* #cliente {
+  color: green;
+  background-color: black;
   max-width: 600px;
   height: 120px;
-}
+} */
+
 /* #clienteNome {
   color: white;
 } */
---
+++ 05-vue.js-data-binding-two-way-binding/src/components/ClienteComp.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="cliente">
+    <!-- Data binding (One-Way Binding) -->
+     <input type="text" :value="nome">
+     <hr>
+     <input type="text" :value="descricao">
+
+    <h2 id="clienteNome">{{ nome }}</h2>
+    <p>Descrição do cliente: {{ descricao }}</p>
+    <!-- Expressões Javascript -->
+    {{ 6 }}
+    {{ 2 + 2 * 2 + 2 }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: "Diego Max",
+      numero: "",
+      email: "",
+      idade: 0,
+      descricao: "Lorem ipsum dolor sit amet",
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* #cliente {
+  color: green;
+  background-color: black;
+  max-width: 600px;
+  height: 120px;
+} */
+
+/* #clienteNome {
+  color: white;
+} */
+</style>
