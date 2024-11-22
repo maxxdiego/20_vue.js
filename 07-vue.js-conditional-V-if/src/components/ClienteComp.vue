03d317ab8e643db14c4010406ffd4d56b7cfdb3a
Diego Max <77diego.max@gmail.com>
2024-11-22 16:41:18 -0300

    @maxxdiego

---
+++ 07-vue.js-conditional-V-if/src/components/ClienteComp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="cliente">
+    <h4>Nome: {{ cliente.nome }}</h4>
+    <p>E-mail: {{ cliente.email }}</p>
+    <!-- <p v-show="showIdade === true">Idade: {{ cliente.idade }}</p> -->
+    <p v-if="showIdade === true">Idade: {{ cliente.idade }}</p>
+    <!-- <p v-else-if="condição">...</p> -->
+    <p v-else>O usuário escondeu a idade!</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    cliente: Object,
+    showIdade: Boolean
+  },
+};
+</script>
+
+<style scoped>
+#cliente {
+  background-color: #ececec;
+  max-width: 600px;
+  padding: 1%;
+  margin-top: 2%;
+}
+
+/* #clienteNome {
+  color: white;
+} */
+</style>
