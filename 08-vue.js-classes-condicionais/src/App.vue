e72a6b089efbd9ad425e4439d8a6297d75187d72
Diego Max <77diego.max@gmail.com>
2024-11-22 16:48:47 -0300

    @maxxdiego

---
+++ 08-vue.js-classes-condicionais/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Clientes:</h1>
+    <!-- <input type="text" v-model="nomeClienteOne"> -->
+    <input type="text" v-model="clienteOne.nome" />
+    <input type="text" v-model="clienteOne.email" />
+    <ClienteComp :cliente="clienteOne" :showIdade="true" />
+    <ClienteComp :cliente="clienteOne" :showIdade="false" />
+    <ClienteComp :cliente="clienteOne" :showIdade="true" />
+    <hr />
+    <h2>Lista de produtos:</h2>
+    <hr />
+    <ProdutoComp />
+    <ProdutoComp />
+  </div>
+</template>
+
+<script>
+import ClienteComp from "./components/ClienteComp.vue";
+import ProdutoComp from "./components/ProdutoComp.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      nomeClienteOne: "Diego Max da Silva",
+      clienteOne: {
+        nome: "Diego Max da Silva",
+        email: "diego@email.com",
+        idade: 18,
+      },
+    };
+  },
+  components: {
+    ClienteComp,
+    ProdutoComp,
+  },
+};
+</script>
+
+<style>
+</style>
